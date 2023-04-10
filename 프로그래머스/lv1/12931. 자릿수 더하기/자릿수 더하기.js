c011652d96ace3b37c2e430e1342894562ca4082
jun yeop lee <104065347+dv-yeop920@users.noreply.github.com>
2023-04-11 02:33:18 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.js
@@ -1,7 +1,6 @@
 function solution(n) {
     let result = 0;
-    var answer;
-    answer = String(n).split('');
+    const answer = String(n).split('');
     answer.map(item => {
         return result += Number(item);
     })
