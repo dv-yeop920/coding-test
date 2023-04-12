c9b1ccb965cb37eafd3731344226177abf4409dc
jun yeop lee <104065347+dv-yeop920@users.noreply.github.com>
2023-04-12 14:38:25 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,9 +1,6 @@
 function solution(numbers) {
     var answer = 0;
     answer = numbers.sort((a,b) => b - a);
-    for(let i = 0; i <= answer.length; i++) {
-        return answer[i] * answer[i+1];
-    }
     
-    return answer;
+    return answer[0] * answer[1];
 }
