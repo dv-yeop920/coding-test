cacccd79553895090f868a2c9c9b7df7f17491df
jun yeop lee <104065347+dv-yeop920@users.noreply.github.com>
2023-04-24 20:07:53 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -2,10 +2,10 @@ function solution(n) {
     var answer = '';
     for(let i = 1; i <= n; i++) {
          if(i % 2 !== 0) {
-            answer += '수'
+            answer += '수';
         }
          if(i % 2 === 0) {
-            answer += '박'
+            answer += '박';
         }
 
     }
