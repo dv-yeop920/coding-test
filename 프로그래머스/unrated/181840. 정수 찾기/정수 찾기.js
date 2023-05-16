7109dedd8c58647271e9ca8c11ea4ad37a6afc18
jun yeop lee <104065347+dv-yeop920@users.noreply.github.com>
2023-05-16 17:59:11 +0900

    [unrated] Title: 정수 찾기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181840. 정수 찾기/정수 찾기.js
@@ -0,0 +1,12 @@
+function solution(num_list, n) {
+    let result = 0;
+    num_list.map(item => {
+        if(item === n ){
+            return result = 1;
+        }
+        else {
+            return result;
+        }
+    })
+    return result;
+}
