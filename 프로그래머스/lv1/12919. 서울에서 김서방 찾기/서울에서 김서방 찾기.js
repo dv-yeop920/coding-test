f8ac9dcedddb73bcb106f4aa5fa0f27772062919
jun yeop lee <104065347+dv-yeop920@users.noreply.github.com>
2023-06-26 00:13:57 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.12 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,9 +1,8 @@
 function solution(seoul) {
-    var answer = '';
+    let answer = '';
     for(let i = 0; i < seoul.length; i++) {
-        seoul[i] === 'Kim' 
-        ? answer = `김서방은 ${seoul.indexOf(seoul[i])}에 있다`
-        :null;
+        if(seoul[i] === 'Kim' ) {
+          return answer = `김서방은 ${seoul.indexOf(seoul[i])}에 있다`;  
+        }
     }
-    return answer;
 }
