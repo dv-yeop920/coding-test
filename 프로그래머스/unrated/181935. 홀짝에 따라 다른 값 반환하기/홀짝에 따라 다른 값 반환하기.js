67f3ec45f220210cf78ac3f66259802c4904b9bd
jun yeop lee <104065347+dv-yeop920@users.noreply.github.com>
2023-07-03 23:18:04 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,17 +1,20 @@
 function solution(n) {
     let answer = 0;
     let answer2 = 0;
+    
     for(let i = 1; i <= n; i++) {
+        
         if(n % 2 === 1) {
             if(i % 2 === 1) {
-            answer += i;
+                answer += i;
+            }
         }
-        }
+        
         if(n % 2 === 0) {
             if(i % 2 === 0) {
                 answer2 = Math.pow(i , 2);
                 answer = answer + answer2;
-        }
+            }
         }
     }
     return answer;
