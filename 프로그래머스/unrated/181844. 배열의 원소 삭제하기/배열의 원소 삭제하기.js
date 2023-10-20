69a683dd84563c7ce27772dbcc0e341cb042946d
jun yeop lee <104065347+dv-yeop920@users.noreply.github.com>
2023-10-20 18:55:42 +0900

    [level unrated] Title: 배열의 원소 삭제하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -1,14 +1,6 @@
 function solution(arr, delete_list) {
-    const result = [];
-    for(let i = 0; i < arr.length; i++) {
-
-        for(let j = 0; j < delete_list.length; j++) {
-
-            if (arr[i] === delete_list[j]) {
-                delete arr[i]
-            }
-        }
-    }
-    arr.map(item => item !== null && result.push(item));
+    const result = arr.filter((item) => {
+        return !delete_list.includes(item);
+    })
     return result;
 }
