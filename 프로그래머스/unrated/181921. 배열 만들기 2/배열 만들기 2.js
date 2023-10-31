017920680cb4fdc4fce4b8b2cdaeb2d2b6ee1721
jun yeop lee <104065347+dv-yeop920@users.noreply.github.com>
2023-11-01 00:46:36 +0900

    [level unrated] Title: 배열 만들기 2, Time: 7.41 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,4 +1,6 @@
 function solution(l, r) {
+    //5 와 0이 포함 안되어 있으면 제외 시키고 5와0만들어간 숫자 푸쉬 하도록
+    //some 함수로 요소가 5와0만 있는지 참거짓 반환 해서 결과값 출력해보기
     const result = [];
 
     for (let i = l; i <= r; i++) {
