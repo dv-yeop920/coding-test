ba883d619dfbef694a1f4f7b035863d61103f6d5
jun yeop lee <104065347+dv-yeop920@users.noreply.github.com>
2023-11-01 01:15:26 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.js
@@ -1,4 +1,9 @@
 function solution(price, money, count) {
+    /*
+    이용료 최초 3원
+    한번 이용하고나면 다음이용료는 최초 이용료의 2배로 이용하도록 (price * 2씩)
+    카운트가 다끝나면 내가 가진 돈(money) - 총 이용료(price)를 가지고 계산 해서 리턴
+    부족하지 않다면 0 리턴*/
     let result;
     let finalprice = price;
     let pirceIncrease = price;
